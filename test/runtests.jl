a8a752583e12a006b74eda3f3b66c00778a78ac0
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2018-08-29 09:53:26 +0200

    bump version to v0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,24 +6,21 @@ if VERSION >= v"0.7.0-DEV.3382"
     using Libdl
 end
 
-Pkg.build("Cairo")  # Tests apt-get code paths
-using Cairo
+#Pkg.build("Cairo")  # Tests apt-get code paths
+#using Cairo
 Pkg.build("HttpParser")  # Tests build-from-source code paths
 using HttpParser
-Pkg.build("GSL")
-using GSL
+#Pkg.build("GSL")
+#using GSL
 
-
-Pkg.build("Gumbo") # Test Autotools code paths
-using Gumbo
-
-
+#Pkg.build("Gumbo") # Test Autotools code paths
+#using Gumbo
 
 # PR 171
 @test BinDeps.lower(nothing, nothing) === nothing
 
 # PR 271
-BinDeps.debug("Cairo")
+# BinDeps.debug("Cairo")
 
 let gv = glibc_version()
     if Compat.Sys.islinux()
